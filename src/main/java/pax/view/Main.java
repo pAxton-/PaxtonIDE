9738f313f4c69abc707434bad3c1d9362047e705
pAxton- <lapxtn@gmail.com>
2016-06-29 20:29:37 -0400

    new look and feel added

--- src/main/java/pax/view/Main.java
+++ src/main/java/pax/view/Main.java
@@ -11,6 +11,23 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
+                try {
+                    // Set System L&F
+                    UIManager.setLookAndFeel(
+                            UIManager.getSystemLookAndFeelClassName());
+                }
+                catch (UnsupportedLookAndFeelException e) {
+                    // handle exception
+                }
+                catch (ClassNotFoundException e) {
+                    // handle exception
+                }
+                catch (InstantiationException e) {
+                    // handle exception
+                }
+                catch (IllegalAccessException e) {
+                    // handle exception
+                }
                 new RootFrame().setVisible(true);
             }
         });
